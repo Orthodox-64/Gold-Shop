f1d2f457b48182cae70da01d2c94fbf676f2b8f5
orthodox-64 <151670745+Orthodox-64@users.noreply.github.com>
2025-04-10 23:23:39 +0530

    Auth & Env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); 
 const express=require("express");
 const mongoose=require("mongoose");
 const {userRouter}=require("./routes/user")
@@ -7,7 +8,7 @@ app.use(express.json());
 app.use("/user",userRouter);
 
 async function main(){
-    await mongoose.connect("mongodb+srv://sachinprogramming62:H3lEBoO8FArmabQs@cluster0.rpgov.mongodb.net/GoldShop");
+    await mongoose.connect(process.env.MONGOURL);
     app.listen(3000,()=>{
         console.log("Server is Running");
     });
